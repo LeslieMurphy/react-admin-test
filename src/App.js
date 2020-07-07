f4a195977dd59e0b9c198100fb51be80b4cc5993
les murphy <les.murphy@eperformancegroup.com>
2020-07-07 13:23:27 -0600

    updated app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource, ListGuesser, EditGuesser  } from 'react-admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import jsonServerProvider from 'ra-data-json-server';
+import UserIcon from '@material-ui/icons/Group';
+
+import TimerIcon from '@material-ui/icons/Timer';
+
+// import { Dashboard } from './dashboard';
+import DashboardTest from './DashboardTest';
+
+
+// const dataProvider = jsonServerProvider('http://localhost:8000');
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+// list={ClockList}  edit={EditGuesser}
+
+const App = () => <Admin 
+        title="TEST Dashboard CSV Upload"
+        dashboard={DashboardTest}
+        dataProvider={dataProvider}
+        > 
+    <Resource name="users" list={ListGuesser} />
+</Admin>;
 
 export default App;
